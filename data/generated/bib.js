530df8697d4f9d992cd0b6d5f9d3ad653455c751
Shaoliang <snie@ncsu.edu>
2019-03-26 17:01:14 -0400

    init

---
+++ data/generated/bib.js
@@ -0,0 +1,48 @@
+﻿define({ entries : {
+    "hohman2018visual": {
+        "abstract": "Deep learning has recently seen rapid development and received significant attention due to its state-of-the-art performance on previously-thought hard problems. However, because of the internal complexity and nonlinear structure of deep neural networks, the underlying decision making processes for why these models are achieving such performance are challenging and sometimes mystifying to interpret. As deep learning spreads across domains, it is of paramount importance that we equip users of deep learning with tools for understanding when a model works correctly, when it fails, and ultimately how to improve its performance. Standardized toolkits for building neural networks have helped democratize deep learning; visual analytics systems have now been developed to support model explanation, interpretation, debugging, and improvement. We present a survey of the role of visual analytics in deep learning research, which highlights its short yet impactful history and thoroughly summarizes the state-of-the-art using a human-centered interrogative framework, focusing on the Five W&#x0027,s and How (Why, Who, What, How, When, and Where). We conclude by highlighting research directions and open research problems. This survey helps researchers and practitioners in both visual analytics and deep learning to quickly learn key aspects of this young and rapidly growing body of research, whose impact spans a diverse range of domains.",
+        "author": "Fred Matthew Hohman and Minsuk Kahng and Robert Pienta and Duen Horng Chau",
+        "doi": "10.1109/TVCG.2018.2843369",
+        "issn": "1077-2626",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "Deep learning,visual analytics,information visualization,neural networks",
+        "month": "",
+        "number": "",
+        "pages": "1-1",
+        "title": "Visual Analytics in Deep Learning: An Interrogative Survey for the Next Frontiers",
+        "type": "article",
+        "volume": "",
+        "year": "2018"
+    },
+    "liu2017towards": {
+        "abstract": "Deep convolutional neural networks (CNNs) have achieved breakthrough performance in many pattern recognition tasks such as image classification. However, the development of high-quality deep models typically relies on a substantial amount of trial-and-error, as there is still no clear understanding of when and why a deep model works. In this paper, we present a visual analytics approach for better understanding, diagnosing, and refining deep CNNs. We formulate a deep CNN as a directed acyclic graph. Based on this formulation, a hybrid visualization is developed to disclose the multiple facets of each neuron and the interactions between them. In particular, we introduce a hierarchical rectangle packing algorithm and a matrix reordering algorithm to show the derived features of a neuron cluster. We also propose a biclustering-based edge bundling method to reduce visual clutter caused by a large number of connections between neurons. We evaluated our method on a set of CNNs and the results are generally favorable.",
+        "author": "Liu, Mengchen and Shi, Jiaxin and Li, Zhen and Li, Chongxuan and Zhu, Jun and Liu, Shixia",
+        "doi": "10.1109/TVCG.2016.2598831",
+        "issn": "1077-2626",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "convolution,data visualisation,directed graphs,edge detection,matrix algebra,neural nets,deep convolutional neural networks,pattern recognition tasks,image classification,high-quality deep models,visual analytics approach,deep CNN,directed acyclic graph,hybrid visualization,hierarchical rectangle packing algorithm,matrix reordering algorithm,neuron cluster,biclustering-based edge bundling method,visual clutter reduction,Neurons,Neural networks,Training,Visual analytics,Clustering algorithms,Image edge detection,Deep convolutional neural networks,rectangle packing,matrix reordering,edge bundling,biclustering",
+        "month": "Jan",
+        "number": "1",
+        "pages": "91-100",
+        "title": "Towards Better Analysis of Deep Convolutional Neural Networks",
+        "type": "article",
+        "volume": "23",
+        "year": "2017"
+    },
+    "strobelt2018lstmvis": {
+        "abstract": "Recurrent neural networks, and in particular long short-term memory (LSTM) networks, are a remarkably effective tool for sequence modeling that learn a dense black-box hidden representation of their sequential input. Researchers interested in better understanding these models have studied the changes in hidden state representations over time and noticed some interpretable patterns but also significant noise. In this work, we present LSTMVis, a visual analysis tool for recurrent neural networks with a focus on understanding these hidden state dynamics. The tool allows users to select a hypothesis input range to focus on local state changes, to match these states changes to similar patterns in a large data set, and to align these results with structural annotations from their domain. We show several use cases of the tool for analyzing specific hidden state properties on dataset containing nesting, phrase structure, and chord progressions, and demonstrate how the tool can be used to isolate patterns for further statistical analysis. We characterize the domain, the different stakeholders, and their goals and tasks. Long-term usage data after putting the tool online revealed great interest in the machine learning community. Recurrent neural networks, and in particular long short-term memory (LSTM) networks, are a remarkably effective tool for sequence modeling that learn a dense black-box hidden representation of their sequential input. Researchers interested in better understanding these models have studied the changes in hidden state representations over time and noticed some interpretable patterns but also significant noise. In this work, we present LSTMVis, a visual analysis tool for recurrent neural networks with a focus on understanding these hidden state dynamics. The tool allows users to select a hypothesis input range to focus on local state changes, to match these states changes to similar patterns in a large data set, and to align these results with structural annotations from their domain. We show several use cases of the tool for analyzing specific hidden state properties on dataset containing nesting, phrase structure, and chord progressions, and demonstrate how the tool can be used to isolate patterns for further statistical analysis. We characterize the domain, the different stakeholders, and their goals and tasks. Long-term usage data after putting the tool online revealed great interest in the machine learning community.",
+        "author": "Strobelt, Hendrik and Gehrmann, Sebastian and Pfister, Hanspeter and Rush, Alexander M",
+        "doi": "10.1109/TVCG.2017.2744158",
+        "issn": "1077-2626",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "data visualisation ,learning (artificial intelligence), recurrent neural nets, statistical analysis, LSTMVis, hidden state dynamics,recurrent neural networks,short-term memory networks,remarkably effective tool,black-box hidden representation,hidden state representations,machine learning community,statistical analysis,chord progressions,phrase structure,nesting,long-term usage data,specific hidden state properties,local state changes,visual analysis tool,Tools,Recurrent neural networks,Visualization,Pattern matching,Computational modeling,Data models,Visualization,Machine Learning,Recurrent Neural Networks,LSTM",
+        "month": "Jan",
+        "number": "1",
+        "pages": "667-676",
+        "publisher": "IEEE",
+        "title": "LSTMVis: A Tool for Visual Analysis of Hidden State Dynamics in Recurrent Neural Networks",
+        "type": "article",
+        "volume": "24",
+        "year": "2018"
+    }
+}});
